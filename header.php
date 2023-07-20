ee4815a47acf0326e7263537a6033c7b1a97b004
sakhasib <adibsakhawat@iut-dhaka.edu>
2023-07-21 01:53:38 +0600

    header php added

---
+++ header.php
@@ -0,0 +1,37 @@
+<!-- Navbar Started -->
+<nav class="navbar navbar-expand-lg bg-dark navbar-dark">
+        <div class="container">
+            <a class="navbar-brand" href="#">
+                <img src="rsx/2@4x.png" alt="Logo" height="40" class="d-inline-block align-text-top">
+            </a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">NoticeBoard</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Standing</a>
+              </li>
+              
+              <li class="nav-item">
+                <a class="nav-link" href="#">About us</a>
+              </li>
+            </ul>
+            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link" aria-current="page" href="#">CR login</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" aria-current="page" href="#">Register</a>
+                </li>
+            </ul>            
+          </div>
+        </div>
+      </nav>
+    <!-- Navbar End -->
