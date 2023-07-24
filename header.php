11cccc35430f50bbb58bfc82653a8e06c9405fd0
sakhasib <adibsakhawat@iut-dhaka.edu>
2023-07-24 15:17:43 +0600

    getting ready for pull

--- header.php
+++ header.php
@@ -25,7 +25,7 @@
             </ul>
             <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
                 <li class="nav-item">
-                    <a class="nav-link" aria-current="page" href="#">CR login</a>
+                    <a class="nav-link" aria-current="page" href="../login/">CR login</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" aria-current="page" href="../signup/">Register</a>
---
+++ login/index.php
@@ -0,0 +1,150 @@
+<?php
+include "../header.php";
+?>
+
+<?php
+// Include the connection file
+require_once "../connection.php";
+
+// Start the session (if not started already)
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+
+// Check if the form is submitted
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    // Get the submitted username and password
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+
+    // Validate inputs (you can add more validation if required)
+    if (empty($username) || empty($password)) {
+        // Redirect back to the login page with an error message
+        header("Location: ../login/index.php?error=empty_fields");
+        exit();
+    } else {
+        // Prepare and execute a secure SELECT query
+        $sql = "SELECT uic, password FROM cr WHERE uic = ?";
+        $stmt = $conn->prepare($sql);
+        $stmt->bind_param("s", $username);
+        $stmt->execute();
+        $result = $stmt->get_result();
+
+        // Check if a user with the given UCI exists
+        if ($result->num_rows === 1) {
+            $row = $result->fetch_assoc();
+            $hashedPassword = $row["password"];
+
+            // Verify the password
+            if (password_verify($password, $hashedPassword)) {
+                // Password is correct, create a session and log in the user
+                $_SESSION["uci"] = $username;
+                header("Location: ../"); // Redirect to the dashboard or home page after successful login
+                exit();
+            } else {
+                // Password is incorrect
+                header("Location: ../login/index.php?error=invalid_password");
+                exit();
+            }
+        } else {
+            // User with the given UCI not found
+            header("Location: ../login/index.php?error=user_not_found");
+            exit();
+        }
+    }
+}
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CR Mate</title>
+
+        <!-- bootstrap -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+        <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+    
+        <!-- Site Icon -->
+         <!-- Favicon -->
+        <link rel="icon" type="image/png" sizes="16x16" href="rsx/1@4x.png">
+        <!-- For high-resolution displays -->
+        <link rel="icon" type="image/png" sizes="32x32" href="rsx/1@4x.png">
+        <link rel="icon" type="image/png" sizes="48x48" href="rsx/1@4x.png">
+        <link rel="icon" type="image/png" sizes="64x64" href="rsx/1@4x.png">
+        <link rel="icon" type="image/png" sizes="128x128" href="rsx/1@4x.png">
+        <!-- For Internet Explorer -->
+        <link rel="icon" type="image/x-icon" href="rsx/1@4x.png">
+        
+        <!-- css -->
+        <link rel="stylesheet" href="style.css">
+    
+        
+        <!-- icons -->
+        <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.8/css/line.css">
+</head>
+<body class="login-body">
+    <!-- Main Login -->
+      <div class="login">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-5 col-12 d-flex flex-column justify-content-center align-items-center sh-boss">
+                    <div class="wrap">
+                        <p class="sh-hd hind text-danger">Log In</p>
+                        <p class="sh-txt hind">Login with your UCI and Password. Please do not share your password 
+                            with anyone else.
+                        </p>
+
+                        <form action="../login/" method="post">
+                            <div class="form-floating mb-3">
+                                <input type="text" class="form-control" id="floatingInput" placeholder="abc" name="username">
+                                <label for="floatingInput">UCI</label>
+                              </div>
+                              <div class="form-floating">
+                                <input type="password" class="form-control" id="floatingPassword" placeholder="Password" name="password">
+                                <label for="floatingPassword">Password</label>
+                              </div>
+                              <div class="spacer"></div>
+                              <button type="submit" class="btn btn-danger align-right hind login-btn" >Login</button>
+                        </form>
+                    </div>
+
+                    <?php
+                        if (isset($_GET["error"])) {
+                            switch ($_GET["error"]) {
+                                case "empty_fields":
+                                    echo '<div class="alert alert-danger" role="alert">
+                                            Input Something
+                                        </div>';
+                                    break;
+                                case "invalid_password":
+                                    echo '<div class="alert alert-danger" role="alert">
+                                            Invalid Password
+                                        </div>';
+                                    break;
+                                case "user_not_found":
+                                    echo '<div class="alert alert-danger" role="alert">
+                                            You Need to register your program first | 
+                                            <a href = "../signup/" class="alert-link">Register</a>
+                                        </div>';
+                                    break;
+                                default:
+                                    echo '<div class="alert alert-danger" role="alert">
+                                            Some error occured
+                                        </div>';
+                                    break;
+                            }
+                        }
+                    ?>
+                </div>
+
+            </div>
+        </div>
+      </div>
+    <!-- Main Login end -->
+    <?php
+        include "../footer.php";
+    ?>
+</body>
+</html>
